560e21338c9f19d3f34818f9862dc4611bd51fc5
giovanni-rojas <gio.rojas15@gmail.com>
2019-07-31 13:58:01 -0700

    added static Scroll module

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CardList from './CardList';
 //import { robots } from './robots';
 import SearchBox from './SearchBox';
 import './App.css';
+import Scroll from './Scroll';
 
 class App extends Component {
 	constructor(){
@@ -36,7 +37,9 @@ class App extends Component {
 				<div className='tc'>
 					<h1 className='f1'>RoboFriends</h1>
 					<SearchBox searchChange={this.onSearchChange}/>	
-					<CardList robots={ filteredRobots }/>			{/* send in state as prop */}
+					<Scroll>
+						<CardList robots={ filteredRobots }/>			{/* send in state as prop */}
+					</Scroll>
 				</div>
 			);
 		}
